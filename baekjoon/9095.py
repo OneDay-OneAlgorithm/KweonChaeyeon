37b304ece9ba4681929e9887552ac410c2188f24
chaeyeon0130 <k84292330@gmail.com>
2024-08-01 23:02:01 +0900

    8/1 9095 1, 2, 3 더하기

---
+++ baekjoon/9095.py
@@ -0,0 +1,15 @@
+import sys
+
+d= [0] * 11
+d[0] = 1
+d[1] = 1
+d[2] = 2
+for i in range(3, 11):
+    d[i] = d[i - 1] + d[i - 2] + d[i - 3]
+    
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    print(d[n])
