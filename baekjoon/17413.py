24cc01c250c87fd661ac5c299ed669d31f018c33
chaeyeon0130 <k84292330@gmail.com>
2024-08-25 17:00:31 +0900

    8/25 17413 단어 뒤집기2 review

--- baekjoon/17413.py
+++ baekjoon/17413.py
@@ -1,33 +1,57 @@
+# import sys
+
+# input = sys.stdin.readline
+
+# sentence = input().strip()
+
+# tagOpen = False
+# stack = []
+
+# for i in sentence:
+#     if i == '<':
+#         while (stack):
+#             print(stack.pop(), end = '')
+#         tagOpen = True
+#         print('<', end = '')
+#     elif i == '>':
+#         tagOpen = False
+#         print('>', end = '')
+#     elif tagOpen == True:
+#         print(i, end = '')
+#     elif tagOpen == False and i != ' ':
+#         stack.append(i)
+#     else:
+#         while (stack):
+#             print(stack.pop(), end = '')
+#         print(' ', end = '')
+
+# # 스택이 비어 있지 않은 경우 체크
+# while (stack):
+#     print(stack.pop(), end = '')    
+# print('') 
+
 import sys
 
 input = sys.stdin.readline
 
-sentence = input().strip()
+s = list(input())
 
-tagOpen = False
 stack = []
-
-for i in sentence:
-    if i == '<':
-        while (stack):
-            print(stack.pop(), end = '')
-        tagOpen = True
-        print('<', end = '')
-    elif i == '>':
-        tagOpen = False
-        print('>', end = '')
-    elif tagOpen == True:
-        print(i, end = '')
-    elif tagOpen == False and i != ' ':
-        stack.append(i)
-    else:
-        while (stack):
-            print(stack.pop(), end = '')
-        print(' ', end = '')
-
-# 스택이 비어 있지 않은 경우 체크
-while (stack):
-    print(stack.pop(), end = '')    
-print('')
-        
-    
+result = ''
+tag_flag = False
+for i in range(len(s) - 1):
+    if s[i] != '<' and tag_flag == False:
+        if s[i] != ' ':
+            stack.append(s[i])
+        if s[i] == ' ' or s[i + 1] == '<' or s[i + 1] == '\n':
+            while stack:
+                result += stack.pop()
+            if s[i] == ' ':
+                result += ' '
+    elif s[i] ==  '<' or tag_flag == True:    
+        result += s[i]
+        if s[i] == '<':
+            tag_flag = True
+        elif s[i] == '>':
+            tag_flag = False
+print(result)
