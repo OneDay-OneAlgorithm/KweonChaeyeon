8c8e5e76603dd30e30c561a8bcb1caaa1ac7a279
chaeyeon0130 <k84292330@gmail.com>
2024-08-26 10:25:59 +0900

    8/26 10799 쇠막대기 review

--- baekjoon/10799.py
+++ baekjoon/10799.py
@@ -1,24 +1,46 @@
+# import sys
+
+# input = sys.stdin.readline
+
+# sentence = input().strip()
+
+# stack = []
+# count = 0
+# index = 0
+
+# for i in sentence:
+#     if i == '(':
+#         stack.append(index)
+#     elif i == ')':
+#         if (stack[-1] == index - 1):
+#             stack.pop()
+#             count += len(stack)
+#         else:
+#             stack.pop()
+#             count += 1
+            
+#     index += 1
+    
+# print(count)
+
 import sys
 
 input = sys.stdin.readline
 
-sentence = input().strip()
+t = input().strip()
 
-stack = []
-count = 0
-index = 0
+result = 0
+bar = 0
+laser = 0
 
-for i in sentence:
-    if i == '(':
-        stack.append(index)
-    elif i == ')':
-        if (stack[-1] == index - 1):
-            stack.pop()
-            count += len(stack)
-        else:
-            stack.pop()
-            count += 1
-            
-    index += 1
-    
-print(count)
+for i in range(len(t)):
+    if t[i] == '(':
+        bar += 1
+    elif t[i] == ')' and t[i - 1] != '(':
+        bar -= 1
+        result += 1
+    elif t[i] == ')' and t[i - 1] == '(':
+        bar -= 1
+        result += bar
+        
+print(result)
